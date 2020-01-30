5c7e7e1b8419abdbf7b0b97a77493e5651438091
wymerkd <wymerkd@gmail.com>
2020-01-29 20:15:31 -0800

    add index.php, style.css, theme image, and test function.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+  function greet($name, $color) {
+    echo "<p>Hello my name is $name and my favorite color is $color.w</p>";
+  }
+  greet('Kyle', 'blue');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+
+<p><?php bloginfo('description'); ?></p>
