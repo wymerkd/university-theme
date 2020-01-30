d75246e47df4a75e599fdbe3b33b3b1944f00759
wymerkd <wymerkd@gmail.com>
2020-01-29 20:36:01 -0800

    add while loop that counts to 99 and while loop that loops through array of names and lists each name with phrase.

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- sample function -->
 <?php
   function greet($name, $color) {
     echo "<p>Hello my name is $name and my favorite color is $color.w</p>";
@@ -5,6 +6,29 @@
   greet('Kyle', 'blue');
 ?>
 
+<!-- sample built in wordpress functions -->
 <h1><?php bloginfo('name'); ?></h1>
 
 <p><?php bloginfo('description'); ?></p>
+
+<!-- sample array with while loop-->
+<?php
+  $names = array('Kyle', 'David', 'Wymer');
+  $count = 0;
+  while($count < count($names)) {
+    echo "<li>Hi, my name is $names[$count]</li>";
+    $count++;
+  }
+?>
+
+<p>Hi, my name is <?php echo $names[0];?>!</p>
+
+<!-- sample loop  -->
+<?php
+  $count = 1;
+
+  while($count < 100) {
+    echo "<li>$count</li>";
+    $count++;
+  }
+?>
